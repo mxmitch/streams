a7f06d67eff7a6a246918ace045b5659b913e254
Mitch Lum <mklum88@gmail.com>
2020-05-26 17:37:43 -0700

    create Google O Auth client and hooked it up to GoogleAuth component

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class GoogleAuth extends React.Component {
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client.init({
+        clientId:
+          "110636906420-62g52cbsgpu4bte426oclqcrpubbbj3d.apps.googleusercontent.com",
+        scope: "email"
+      });
+    });
+  }
+
+  render() {
+    return <div>Google Auth</div>;
+  }
+}
+
+export default GoogleAuth;
